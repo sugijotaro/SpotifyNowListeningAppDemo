6a4332cc2b3cb35b2af359872f4705e5661c4862
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-07-09 10:55:03 +0900

    Initial Commit

---
+++ SpotifyNowListeningAppDemo/SpotifyNowListeningAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyNowListeningAppDemoApp.swift
+//  SpotifyNowListeningAppDemo
+//
+//  Created by JotaroSugiyama on 2023/07/09.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyNowListeningAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
